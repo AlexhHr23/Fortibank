39638e71afcad5b6dead4eaab2a3133be30fb174
Alexhr <alex.hernandez24kb@gmail.com>
2024-05-02 10:55:22 -0600

    Creacion de modelo Usuario

--- fortibank/settings.py
+++ fortibank/settings.py
@@ -135,6 +135,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'userauths.User'
+
 
 JAZZMIN_SETTINGS = {
     # title of the window (Will default to current_admin_site.site_title if absent or None)
