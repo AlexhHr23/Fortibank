3315c363099fded6e58777967d0ca62562bfb66c
Alexhr <alex.hernandez24kb@gmail.com>
2024-05-02 12:55:28 -0600

    Autenticación de registros

--- userauths/views.py
+++ userauths/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from userauths.forms import UserRegisterForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
-# Create your views here.
+
+def RegisterView(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            #form.save()
+            new_user = form.save() #new_user.email
+            username = form.cleaned_data.get("username")
+            username = request.POST.get("username")
+            messages.success(request, f"Hey {username} la cuenta fue creada correctamente.")
+            new_user = authenticate(usermane=form.cleaned_data["email"], 
+                                    password=form.cleaned_data["password1"])
+            login(request, new_user)
+            return redirect("core:index")
+        
+    if request.user.is_authenticated:
+        messages.warning(request, f"Hey {username} estas loguado correctamente !")
+        return redirect("core:index")
+    
+    else:
+        form = UserRegisterForm()
+    context = {
+        "form": form
+    }
+    return render(request, "userauths/sign-up.html", context)
