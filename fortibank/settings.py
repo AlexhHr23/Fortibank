c367b8c5346c92ebf1e6b5fcd14e8adf486942e3
Alexhr <alex.hernandez24kb@gmail.com>
2024-05-02 10:20:03 -0600

    Traducciónde pagína al español

--- fortibank/settings.py
+++ fortibank/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
@@ -137,14 +137,34 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 JAZZMIN_SETTINGS = {
-    # "site_title": "Moviex"
-    "site_header": "Administrador",
+    # title of the window (Will default to current_admin_site.site_title if absent or None)
+    "site_title": "Fortibank",
+
+    # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_header": "Fortibank",
+
+    # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_brand": "Fortibank",
+
+    # Logo to use for your site, must be present in static files, used for brand on top left
+    #"site_logo": "books/img/logo.png",
+
+    # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
     "login_logo": True,
-  
-    "side_brand": "Plataforma líder de apuestas políticas",
+
+    # Logo to use for login form in dark themes (defaults to login_logo)
+    "login_logo_dark": None,
+
+    # CSS classes that are applied to the logo above
+    "site_logo_classes": "img-circle",
+
+    # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
+    "site_icon": True,
+
+    # Welcome text on the login screen
     "welcome_sign": "Bienvenido",
 
-    # "side_logo": "images/ logo.jpg",
+    # Copyright on the footer
     "copyright": "AB FORTI - Todos los derechos reservados @ Copyrigth 2024 - Hasta la fecha",
     #"order_with_respect_to": ["core", "userauths", "transactions", "addon", "blog"]
 }
