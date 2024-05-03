8a6e0fb783bf5956db20a78cd3b7fe803932115d
Alexhr <alex.hernandez24kb@gmail.com>
2024-05-03 12:53:17 -0600

    Correción de errroes e implementacion de login

--- userauths/views.py
+++ userauths/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from userauths.forms import UserRegisterForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 
@@ -11,15 +11,16 @@ def RegisterView(request):
             #form.save()
             new_user = form.save() #new_user.email
             username = form.cleaned_data.get("username")
-            username = request.POST.get("username")
+            #username = request.POST.get("username")
             messages.success(request, f"Hey {username} la cuenta fue creada correctamente.")
-            new_user = authenticate(usermane=form.cleaned_data["email"], 
-                                    password=form.cleaned_data["password1"])
+            #new_user = authenticate(username=form.cleand_data.get('email'))
+            new_user = authenticate(username=form.cleaned_data['email'], 
+                                    password=form.cleaned_data['password1'])
             login(request, new_user)
             return redirect("core:index")
         
     if request.user.is_authenticated:
-        messages.warning(request, f"Hey {username} estas loguado correctamente !")
+        messages.warning(request, f"Hey correcto.")
         return redirect("core:index")
     
     else:
