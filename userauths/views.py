0030f198221cdb9b969549ccef4d1427dc14a211
Alexhr <alex.hernandez24kb@gmail.com>
2024-05-03 16:47:29 -0600

    Implementación de creación e logueo de usuarios

--- userauths/views.py
+++ userauths/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from userauths.forms import UserRegisterForm
 from django.contrib.auth import authenticate, login, logout
+
+from userauths.models import User
 from django.contrib import messages
 
 
@@ -29,3 +31,25 @@ def RegisterView(request):
         "form": form
     }
     return render(request, "userauths/sign-up.html", context)
+
+
+def LoginView(request): 
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        
+        try:
+            user = User.objects.get(email=email)
+            user = authenticate(request, email=email, password=password)
+            
+            if user is not None:
+                login(request, user)
+                messages.success(request, "Inicio de sesión exitoso.")
+                return redirect("core:index")
+            else:
+                messages.warning(request, "usuario o contraseña no es correcto")
+                return redirect("userauths:sign-in")
+        except: 
+            messages.warning(request, "No existe el usuario")
+            
+    return render(request, "userauths/sign-in.html")
