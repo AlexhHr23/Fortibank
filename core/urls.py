1bd7039552bd515fca7412fbd6c513aa2dfe34e2
Alexhr <alex.hernandez24kb@gmail.com>
2024-05-10 14:43:09 -0600

    Implementación del buscador

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from core import views
+from core import transfer
 
 app_name = "core"
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    
+    #Transfers
+    path("search-account/", transfer.search_users_account_number, name="search-account")
 ]
