67b8007c3746259e451e8330a6d9f12cd454877c
Alexhr <alex.hernandez24kb@gmail.com>
2024-05-15 15:33:44 -0600

    Implementación de Página de confirmación de solicitud de pago

--- core/payment_request.py
+++ core/payment_request.py
@@ -35,34 +35,44 @@ def AmountRequest(request, account_number):
 
 def AmountRequestProcess(request, account_number):
     account = Account.objects.get(account_number=account_number)
-    
+
     sender = request.user
     reciever = account.user
-    
+
     sender_account = request.user.account
     reciever_account = account
-    
+
     if request.method == "POST":
         amount = request.POST.get("amount-request")
         description = request.POST.get("description")
-        
+
         new_request = Transaction.objects.create(
             user=request.user,
             amount=amount,
             description=description,
-            
+
             sender=sender,
             reciever=reciever,
-            
+
             sender_account=sender_account,
             reciever_account=reciever_account,
-            
-            status="requested",
+
+            status="request_processing",
             transaction_type="request"
         )
         new_request.save()
-        transaction_id= new_request.transaction_id
-        return redirect("core:request-confirmation", account.account_number, transaction_id)
+        transaction_id = new_request.transaction_id
+        return redirect("core:amount-request-confirmation", account.account_number, transaction_id)
     else:
-        messages.warning(request, "Ocurrio un error, intentalo más tarde")        
-        return redirect("core:dashboard")
+        messages.warning(request, "Error Occured, try again later.")
+        return redirect("account:dashboard")
+
+def AmountRequestConfirmation(request, account_number, transaction_id):
+    account = Account.objects.get(account_number=account_number)
+    transaction = Transaction.objects.get(transaction_id=transaction_id)
+
+    context = {
+        "account":account,
+        "transaction":transaction,
+    }
+    return render(request, "payment_request/amount-request-confirmation.html", context)
