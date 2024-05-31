ec135693fe19ca59eb569be1da449037409081bd
Josue Recoba Huerta <josue.huer.reco.22@gmail.com>
2024-05-31 12:24:36 -0600

    Creacion de alertas en pantalla de Regristro de usuario

--- userauths/views.py
+++ userauths/views.py
@@ -2,62 +2,73 @@ from os import name
 from django.shortcuts import render, redirect
 from userauths.forms import UserRegisterForm
 from django.contrib.auth import authenticate, login, logout
-
 from userauths.models import User
 from django.contrib import messages
+from django.core.exceptions import ValidationError
+from django.contrib.auth.password_validation import validate_password
+
+from django.contrib.auth import authenticate, login, password_validation
 
 
 def RegisterView(request):
     if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            #form.save()
-            new_user = form.save() #new_user.email
-            username = form.cleaned_data.get("username")
-            #username = request.POST.get("username")
-            messages.success(request, f"Hey {username} la cuenta fue creada correctamente.")
-            #new_user = authenticate(username=form.cleand_data.get('email'))
-            new_user = authenticate(phone_number=form.cleaned_data['phone_number'], 
-                                    password=form.cleaned_data['password1'])
-            login(request, new_user)
-            return redirect("account:account")
-        
+            password1 = form.cleaned_data.get('password1')
+            password2 = form.cleaned_data.get('password2')
+            
+            if password1 != password2:
+                messages.error(request, "Las contraseñas no coinciden. Por favor, inténtalo de nuevo.")
+            else:
+                try:
+                    validate_password(password1)
+                    new_user = form.save()
+                    username = form.cleaned_data.get("username")
+                    messages.success(request, f"Hey {username}, la cuenta fue creada correctamente.")
+                    new_user = authenticate(phone_number=form.cleaned_data['phone_number'], password=password1)
+                    login(request, new_user)
+                    return redirect("account:account")
+                except ValidationError:
+                    messages.error(request, "La contraseña es demasiado débil. Debe contener al menos 8 caracteres incluyendo números y letras .")
+
+        else:
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, error)
+
     if request.user.is_authenticated:
-        messages.warning(request, f"Hey correcto.")
+        messages.warning(request, "Ya has iniciado sesión.")
         return redirect("account:account")
-    
     else:
         form = UserRegisterForm()
-    context = {
-        "form": form
-    }
+
+    context = {"form": form}
     return render(request, "userauths/sign-up.html", context)
-
-
-def LoginView(request): 
+def LoginView(request):
     if request.method == "POST":
         phone_number = request.POST.get("phone_number")
         password = request.POST.get("password")
-        
+
         try:
             user = User.objects.get(phone_number=phone_number)
             user = authenticate(request, phone_number=phone_number, password=password)
-            
+
             if user is not None:
                 login(request, user)
                 messages.success(request, "Inicio de sesión exitoso.")
                 return redirect("account:account")
             else:
-                messages.warning(request, "usuario o contraseña no es correcto")
+                messages.warning(request, "Usuario o contraseña no es correcto")
                 return redirect("userauths:sign-in")
-        except: 
+        except:
             messages.warning(request, "No existe el usuario")
-        
+
     if request.user.is_authenticated:
         messages.warning(request, "Has iniciado sesión.")
         return redirect("account:account")
-    
+
     return render(request, "userauths/sign-in.html")
+
 
 def LogoutView(request):
     logout(request)
