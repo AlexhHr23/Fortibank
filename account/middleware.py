d7c271a4922a361ec3c8edeb24f42fbafc6f6f5e
Alexhr <alex.hernandez24kb@gmail.com>
2024-05-31 00:44:29 -0600

    Correción en la ruta admin

--- account/middleware.py
+++ account/middleware.py
@@ -1,6 +1,5 @@
 from django.shortcuts import redirect
 
-
 class CustomErrorMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
@@ -10,7 +9,12 @@ class CustomErrorMiddleware:
 
         # Si la respuesta es un error 404 (página no encontrada)
         if response.status_code == 404:
-            # Redirige al dashboard principal
+            # Verifica si la URL solicitada comienza con 'admin/'
+            if request.path.startswith('/admin'):
+                # Permite el acceso a la ruta /admin/
+                return response
+
+            # Redirige al dashboard principal en otros casos
             return redirect('account:dashboard')
 
         return response
