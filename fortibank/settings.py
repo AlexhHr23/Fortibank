d75e3333da41fb75d6e0985e660bf2961d6b8963
Alexhr <alex.hernandez24kb@gmail.com>
2024-06-01 01:32:12 -0600

    Cambios en la configuración para el despliegue

--- fortibank/settings.py
+++ fortibank/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-289g*jy(iu#j$fn_4zy%bmj@d*_z_ou)+!j525!i=4e5ed0f)d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
