afcf4113561ab7b3947e79229d18788342ab2491
Alexhr <alex.hernandez24kb@gmail.com>
2024-06-02 14:01:43 -0600

    Correción de gitignore y borrar migraciones

--- fortibank/settings.py
+++ fortibank/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-289g*jy(iu#j$fn_4zy%bmj@d*_z_ou)+!j525!i=4e5ed0f)d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =False
+DEBUG=False
 
 ALLOWED_HOSTS = ['185.164.111.40', 'forti-bank.com', 'www.forti-bank.com']
 
